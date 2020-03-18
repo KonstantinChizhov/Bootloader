d61933d54debcfa258d170266dc85786ad855846
Konstantin Chizhov <kchizhov@mirantis.com>
2020-03-18 15:12:55 +0400

    Finalize uploader script

--- boot_test.py
+++ boot_test.py
@@ -6,70 +6,107 @@ from pymodbus.client.sync import ModbusSerialClient as ModbusClient
 from pymodbus.exceptions import ModbusException
 
 modbusAddr = 1
-PageBufferSize = 2048
+PageBufferSize = 64
 PageBufferAddr = 1024
 PageMapAddr = 1024
 CommandAddress = 100
 CommandParamsSize = 100
 
+ParamPage = 0
+ParamOffset = 1
+ParamLength = 2
+ParamCommand = 3
+
+CommandNone = 0
+CommandPageErase = 1
+CommandPageWrite = 2
+CommandPageRead = 3
+CommandRunApplication = 4
+CommandReset = 5
+
+modAddr = 1
+retries = 3
+
+def WriteRegs(clinet, addr, values):
+	rr = None
+	for attempt in range(0, retries):
+		rr = client.write_registers(addr, values, unit=modAddr)
+		if not isinstance(rr, ModbusException):
+			return rr
+	raise rr
+
+def ReadHoldingRegs(clinet, addr, count):
+	rr = None
+	for attempt in range(0, retries):
+		rr = client.read_holding_registers(addr, count, unit=modAddr)
+		if not isinstance(rr, ModbusException):
+			return rr
+	raise rr
+
+def ReadInputRegs(clinet, addr, count):
+	rr = None
+	for attempt in range(0, retries):
+		rr = client.read_input_registers(addr, count, unit=modAddr)
+		if not isinstance(rr, ModbusException):
+			return rr
+	raise rr
+
 def GetDeviceName(client):
-	rr = client.read_holding_registers(0, 16, unit=1)
-	if isinstance(rr, ModbusException):
-		raise rr
+	rr = ReadHoldingRegs(client, 0, 16)
 	return ''.join(chr(i) for i in rr.registers)
 
 def GetMcuName(client):
-	rr = client.read_holding_registers(16, 16, unit=1)
-	if isinstance(rr, ModbusException):
-		raise rr
+	rr = ReadHoldingRegs(client, 16, 16)
 	return ''.join(chr(i) for i in rr.registers)
 
-def AppendInt(a, b):
-	return 
-
 def GetMcuId(client):
-	rr = client.read_holding_registers(34, 6, unit=1)
-	if isinstance(rr, ModbusException):
-		raise rr
+	rr = ReadHoldingRegs(client, 34, 6)
 	result = 0
 	for i in rr.registers:
 		result = (result << 16) | i
 	return result
 
 def GetBootVersionCount(client):
-	rr = client.read_holding_registers(32, 1, unit=1)
-	if isinstance(rr, ModbusException):
-		raise rr
+	rr = ReadHoldingRegs(client, 32, 1)
 	return rr.registers[0]
 
 def WritePage(client, data, page, offset):
+	modbusData = []
+	for i in range(0, len(data), 2):
+		modbusData.append(data[i] + (data[i + 1] << 8))
+	
+	rr = WriteRegs(client, PageBufferAddr, modbusData)
+	data = [page, offset, len(data), CommandPageWrite]
+	rr = WriteRegs(client, CommandAddress, data)
 	return 0
 
 
 def ErasePage(client, page):
+	data = [page, 0, 0, CommandPageErase]
+	rr = WriteRegs(client, CommandAddress, data)
 	return 0
 
+def Reset(client):
+	data = [0, 0, 0, CommandReset]
+	rr = client.write_registers(CommandAddress, data, unit=modAddr)
+	rr = client.write_registers(CommandAddress, data, unit=modAddr)
+	# ignore errors here, bootlodaer will not responce anyway
+	return 0
 
 def GetPageCount(client):
-	rr = client.read_holding_registers(43, 2, unit=1)
-	if isinstance(rr, ModbusException):
-		raise rr
+	rr = ReadHoldingRegs(client,43, 2)
 	result = rr.registers[0]
 	if result < 4 or result > 8*1024:
 		raise Exception('Unexpected page count: %d' % result)
 	return rr.registers[0], rr.registers[1]
 
 def GetFlashSize(client):
-	rr = client.read_input_registers(45, 2, unit=1)
-	if isinstance(rr, ModbusException):
-		raise rr
+	rr = ReadHoldingRegs(client, 45, count=2)
 	result = rr.registers[0] | (rr.registers[1] << 16)
 	return result
 
 def GetPageSize(client, page):
-	rr = client.read_input_registers(page * 4 + 2, 2, unit=1)
-	if isinstance(rr, ModbusException):
-		raise rr
+	rr = ReadInputRegs(client, page * 4 + 2, 2)
 	result = rr.registers[0] | (rr.registers[1] << 16)
 	if result < 1024 or result > 256*1024:
 		raise Exception('Unexpected page %d size: %d' % (page, result))
@@ -77,9 +114,7 @@ def GetPageSize(client, page):
 
 
 def GetPageAddress(client, page):
-	rr = client.read_input_registers(page * 4, 2, unit=1)
-	if isinstance(rr, ModbusException):
-		raise rr
+	rr = ReadInputRegs(client, page * 4, 2)
 	result = rr.registers[0] | (rr.registers[1] << 16)
 	if result < 0x08000000 or result > 0x08000000 + 4*1024*1024:
 		raise Exception('Unexpected page %d address: %x' % (page, result))
@@ -87,22 +122,35 @@ def GetPageAddress(client, page):
 
 
 def BootInit():
-	client = ModbusClient(method='rtu', port='COM7', stopbits=1, timeout=1, baudrate=115200)
+	client = ModbusClient(method='rtu', port='COM4', stopbits=1, timeout=1, baudrate=115200)
 	client.connect()
 	return client
 
 
 def BootPrettyWritePage(client, data, page, offset):
-	print('Writing page #%u (%u bytes)\t' % (page, len(pageData)))
+	print('Writing page #%u (%u bytes)\t' % (page, len(data)))
 
 	error = ErasePage(client, page)
 	if error != 0:
-		raise Exception('Error erasing page. ErrCode = 0x%04x' % error)
-	error = WritePage(client, data, page, offset)
-	if error != 0:
-		raise Exception('Error writing page. ErrCode = 0x%04x' % error)
-	print('OK')
+		raise Exception('Error erasing page. ErrCode = %s' % error)
+	chunkSize = PageBufferSize
+	wordsWritten = 0
+	for chunk in range(offset, offset + len(data), chunkSize):
+		chunkData = data[chunk:chunk+chunkSize]
+		print('Writing chunk #%u (%u bytes)\t' % (chunk, len(chunkData)))
+		error = WritePage(client, chunkData, page, chunk)
+		if error != 0:
+			raise Exception('Error writing page. ErrCode = %s' % error)
 
+		wordsWritten += chunkSize
+	
+	# writing rest of page
+	chunkData = data[wordsWritten:-1]
+	print('Writing last chunk #%u (%u bytes)\t' % (chunk, len(chunkData)))
+	error = WritePage(client, chunkData, page, wordsWritten)
+	if error != 0:
+		raise Exception('Error writing page. ErrCode = %s' % error)
+	print('OK')
 
 
 if len(sys.argv) <= 1:
@@ -130,6 +178,10 @@ pageCount = GetPageCount(client)
 print ('Flash page count: %u (%u)' % pageCount)
 print ('Total Flash size: %u' % (GetFlashSize(client) ))
 
+# for i in range(0, pageCount[0]):
+# 	pageSize = GetPageSize(client, i)
+# 	print ("page %i size = %i" % (i, pageSize))
+
 page = 0
 pageSize = GetPageSize(client, page)
 pageEnd = GetPageAddress(client, page) + pageSize
@@ -147,11 +199,13 @@ for addr, value in hexItems.items():
 		pageData = []
 	pageData.append(value)
 
+# align data to be written on 8 byte boundary
 align = (8 - (len(pageData) & 7)) & 7
 for i in range(align):
 	pageData.append(0)
 
 # write last page
 BootPrettyWritePage(client, pageData, page, 0)
-
-
+print("Success")
+print("Reseting system")
+Reset(client)
